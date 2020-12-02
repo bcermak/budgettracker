f2f05cc6edace005f1ee4ecf278203e84d7316eb
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-02 15:07:53 -0600

    making server changes to try and fix deployment issue

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(`mongodb+srv://bcermak:${process.env.mongopass}@cluster0.t0gse.mongodb.net/budget?retryWrites=true&w=majority` || "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
@@ -37,21 +33,4 @@ app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
-});
-
-
-db.newdatabase.insert({
-  "name2": "this is name2",
-  "title": ["item 1", "item2", "item3"],
-  "country": "this is country"
-})
-
-app.get("/all", (req, res) => {
-  db.newdatabase.find({}, (error, found) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.json(found);
-    }
-  });
 });
